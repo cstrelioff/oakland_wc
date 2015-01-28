e4a9c43b72527f6f2a15cb4e83ee2e2b2ea3087d
Christopher C Strelioff <chris.strelioff@gmail.com>
2015-01-28 12:18:19 -0800

    first commit js/wc.js

---
+++ js/wc.js
@@ -0,0 +1,47 @@
+/*
+ * oakland_pubrests.js
+ * Copyright (C) 2015 Christopher C. Strelioff <chris.strelioff@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'examples.map-i875mjb7'
+}).addTo(map);
+
+// Locate User
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+
+// Mark bathrooms
+L.marker([37.80741, -122.25427]).addTo(map)
+    .bindPopup("<b>Bathroom 1</b>");
+
+// Show click location
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
